0db282eaa72c82188341be63702e01cc7c43352d
BeobSik <tjswl756@naver.com>
2021-12-04 14:15:17 +0900

    feat: init Kiosk main

--- Kiosk/src/Main.java
+++ Kiosk/src/Main.java
@@ -1,5 +1,26 @@
+import Component.Component;
+
 public class Main {
+    private static String phoneNo;
+    private static String userType;
+    private static View view;
+    private static Component component;
+    private static Recipe recipe;
+
     public static void main(String[] args) {
+
+    }
+
+    private static String login(String phoneNo){
+
+        return "Y";
+    }
+
+    private static void deleteCustomerInfo(String phoneNo){
+
+    }
+
+    private static void logout(){
 
     }
 }
