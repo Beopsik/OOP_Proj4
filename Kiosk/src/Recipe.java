cee2d4eae2a9cc69fc6b8a90b853486c8acf0c9c
BeobSik <tjswl756@naver.com>
2021-12-04 14:21:52 +0900

    feat: init Kiosk Recipe

--- Kiosk/src/Recipe.java
+++ Kiosk/src/Recipe.java
@@ -1,2 +1,25 @@
+import Component.Component;
+
+import java.util.ArrayList;
+
 public class Recipe {
+    private View view;
+    private ArrayList<Component> components;
+    private Order order;
+
+
+    public void addRecipes(){
+    }
+    public void saveRecipe(String recipe){
+
+    }
+    public String loadRecipe(String phoneNo){
+        return "result";
+    }
+    public void modifyRecipes(){
+
+    }
+    public void deleteRecipes(){
+
+    }
 }
